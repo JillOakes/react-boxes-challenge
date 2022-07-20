ba3b6ca447f2f573111978bda351163009c816a0
Jill Oakes <jill.oakes@beckshybrids.com>
2022-07-20 17:08:46 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import boxes from "./boxes";
+import Box from "./Box";
 
 function App() {
+  // Initialize state with the default value of the array pulled in from boxes.js
+  const [squares, setSquares] = React.useState(boxes);
+
+  // This is the imperative way of writing it, next will be the declarative way of writing it.
+  function toggle(id) {
+    setSquares((prevSquares) => {
+      const newSquares = [];
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
+  }
+
+  // Map over the state array and display each one as a square
+  const squareElements = squares.map((square) => (
+    <Box key={square.id} id={square.id} on={square.on} toggle={toggle} />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>{squareElements}</main>
     </div>
   );
 }
